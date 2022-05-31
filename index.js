d57ab0d416953d4d9b5581f772908441eeab8c32
Jared Grippe <jared@deadlyicon.com>
2022-05-31 10:17:21 -0700

    got basic encrypted key/value storage working

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 const Debug = require('debug')
 const Path = require('path')
-const safetyCatch = require('safety-catch')
+// const safetyCatch = require('safety-catch')
 const sodium = require('sodium-universal')
 const b4a = require('b4a')
 const fs = require('fs/promises')
+const mkdirp = require('mkdirp-classic')
 const cenc = require('compact-encoding')
 
 const debug = Debug('jlinx:vault')
 
-const VERSION = 1
-const KEY_CHECK_VALUE = b4a.from('KEY_CHECKS_OUT')
+const VERSION = '1'
+const KEY_CHECK_VALUE = 'KEY_CHECKS_OUT'
 
-const ENCODINGS = [
+const ENCODERS = [
   {
     name: 'raw',
     prefix: b4a.from('0'),
@@ -21,7 +22,7 @@ const ENCODINGS = [
   {
     name: 'string',
     prefix: b4a.from('1'),
-    encode: x => cenc.encode(cenc.string, x),
+    encode: x => cenc.encode(cenc.string, x.toString()),
     decode: x => cenc.decode(cenc.string, x)
   },
   {
@@ -61,6 +62,8 @@ module.exports = class JlinxVault {
       if (!b4a.equals(KEY_CHECK_VALUE, keyCheck)) {
         throw new Error('invalid vault key')
       }
+    } else {
+      await this.init()
     }
   }
 
@@ -79,43 +82,47 @@ module.exports = class JlinxVault {
   }
 
   async init () {
-    await fs.mkdir(this.storagePath).catch(safetyCatch)
+    await mkdirp(this.path)
     await this.set('VERION', VERSION)
     await this.set('KEY_CHECK', KEY_CHECK_VALUE)
   }
 
   async _keyToPath (key) {
-    console.log(this, this.crypto)
-    const encryptedPath = this.crypto.encrypt(key)
-    const asHex = encryptedPath.toString('hex')
+    debug('_keyToPath', { key })
+    key = b4a.from(key)
+    const encryptedKey = this.crypto.encrypt(key, key)
+    const asHex = encryptedKey.toString('hex')
+    debug('encryptedKey', asHex)
+    const decryptedKey = this.crypto.decrypt(b4a.from(asHex, 'hex'), key)
+    debug('decryptedKey', decryptedKey.toString(), b4a.equals(decryptedKey, key))
     return Path.join(
+      this.path,
       asHex.slice(0, 2),
       asHex.slice(2, 4),
       asHex
     )
   }
 
-  _getEncoder ({ encoding, prefix }) {
-    if (!(encoding in ENCODINGS)) {
-      throw new Error(`invalid encoding ${encoding}`)
-    }
-    const [code, encoder] = ENCODINGS[encoding]
-    return {
-      prefix: b4a.from(`${code}`),
-      encoder
-    }
-  }
-
   async set (key, value, encoding = 'string') {
-    const encoder = this._getEncoder({ encoding })
-    console.log(encoder)
+    const encoder = ENCODERS.find(encoder => encoder.name === encoding)
+    if (!encoder) {
+      throw new Error(`invalid encoding "${encoding}"`)
+    }
     const path = await this._keyToPath(key)
-    const encoded = encoder.encode(encoder, value)
-    const encrypted = this.crypto.encrypt(
-      b4a.contact([encoder.prefix, encoded])
-    )
+    const encoded = encoder.encode(b4a.from(value))
+    const decrypted = b4a.concat([encoder.prefix, encoded])
+    const encrypted = this.crypto.encrypt(decrypted, key)
     debug('set', { key, path })
-    await this.writeFile(path, encrypted)
+    await new Promise((resolve, reject) => {
+      const dirPath = Path.dirname(path)
+      debug('mkdir', dirPath)
+      mkdirp(dirPath, error => {
+        if (error) { reject(error) } else { resolve() }
+      })
+    })
+    await fs.writeFile(path, encrypted)
+    debug('set success', key)
+    return true
   }
 
   async get (key) {
@@ -123,36 +130,64 @@ module.exports = class JlinxVault {
     debug('get', { key, path })
     let encrypted
     try {
-      encrypted = await fs.readFile(Path.join(this.path, path))
+      encrypted = await fs.readFile(path)
     } catch (error) {
       if (error && error.code === 'ENOENT') return
       throw error
     }
     if (!encrypted) return
-    const decrypted = this.crypto.decrypt(encrypted)
+    const decrypted = this.crypto.decrypt(encrypted, key)
     const prefix = decrypted.slice(0, 1)
     const encoded = decrypted.slice(1)
-    const encoder = this._getEncoder({ prefix })
+    const encoder = ENCODERS.find(encoder =>
+      b4a.equals(encoder.prefix, prefix)
+    )
+    if (!encoder) {
+      throw new Error(`unkown encoding prefix="${prefix}"`)
+    }
+    debug('get using encoder', encoder)
     const value = encoder.decode(encoded)
     return value
   }
 
   async has (key) {
-
+    return typeof (await this.get(key)) !== 'undefined'
   }
 
   async delete (key) {
-
+    debug('delete', { key })
+    const path = await this._keyToPath(key)
+    debug('delete', { path })
+    try {
+      await fs.unlink(path)
+      return true
+    } catch (error) {
+      if (error.code === 'ENOENT') return false
+      throw error
+    }
   }
 }
 
 function makeCrypto (key) {
   return {
-    encrypt () {
-      throw new Error('NOT DONT YET')
+    encrypt (decrypted, name) {
+      const nonce = deriveNonce(key, name)
+      const encrypted = Buffer.alloc(decrypted.length + sodium.crypto_secretbox_MACBYTES)
+      sodium.crypto_secretbox_easy(encrypted, decrypted, nonce, key)
+      return encrypted
     },
-    decrypt () {
-      throw new Error('NOT DONT YET')
+    decrypt (encrypted, name) {
+      const nonce = deriveNonce(key, name)
+      const decrypted = b4a.alloc(encrypted.length - sodium.crypto_secretbox_MACBYTES)
+      sodium.crypto_secretbox_open_easy(decrypted, encrypted, nonce, key)
+      return decrypted
     }
   }
+}
+
+function deriveNonce (key, name) {
+  if (!b4a.isBuffer(name)) name = b4a.from(name)
+  const out = b4a.alloc(sodium.crypto_secretbox_NONCEBYTES)
+  sodium.crypto_generichash_batch(out, [b4a.from('jlinx rules'), name], key)
+  return out
 }
--- test/simple.js
+++ test/simple.js
@@ -12,29 +12,23 @@ test('simple', async t => {
       path: dir.path,
       key
     })
-    console.log(vault)
     await vault.ready()
-    console.log(vault)
 
-    // const signingKeyPair = createSigningKeyPair()
+    t.same(await vault.get('emptykey'), undefined)
+    t.same(await vault.has('emptykey'), false)
+    t.same(await vault.set('name', 'Jared'), true)
+    t.same(await vault.has('name'), true)
+    t.same(await vault.get('name'), 'Jared')
+    t.same(await vault.delete('name'), true)
+    t.same(await vault.has('name'), false)
+    t.same(await vault.delete('name'), false)
 
-    // t.same(await keys.has(signingKeyPair.publicKey), false)
-    // t.same(await keys.get(signingKeyPair.publicKey), undefined)
-    // t.same(await keys.put(signingKeyPair), true)
-    // t.same(await keys.has(signingKeyPair.publicKey), true)
-    // t.same(
-    //   await keys.get(signingKeyPair.publicKey),
-    //   signingKeyPair.secretKey
-    // )
-    // t.same(await keys.delete(signingKeyPair.publicKey), true)
-    // t.same(await keys.has(signingKeyPair.publicKey), false)
-    // t.same(await keys.get(signingKeyPair.publicKey), undefined)
-
-    // const encryptingKeyPair = createEncryptingKeyPair()
-    // t.same(await keys.put(encryptingKeyPair), true)
-    // t.same(
-    //   await keys.get(encryptingKeyPair.publicKey),
-    //   encryptingKeyPair.secretKey
-    // )
+    for (let i = 0; i < 10; i++) {
+      await vault.set(`${i}`, `${i * 10}`)
+    }
+    t.same(await vault.get('0'), '0')
+    t.same(await vault.get('1'), '10')
+    t.same(await vault.get('5'), '50')
+    t.same(await vault.get('9'), '90')
   })
 })
